9e038be5249b0b4db2f7b981026e9c30ea50780a
linesdev-mahmoud <mahmag9348@gmail.com>
2022-12-04 11:29:00 +0200

    hazem >>> GN_GLOBAL_PKG  service allowed

--- Packages/HCM7_DEV.sql
+++ Packages/HCM7_DEV.sql
@@ -14,3 +14,48 @@
 --> Add GN_TRANSLATION_API.AMS_ACCRUAL_PLANS_JSON_TL     
 --> Add GN_TRANSLATION_PKG.DECODE_AMS_ACCRUAL_PLANS_TL 
 --> update GN_GENERAL_API.GN_LOOKUP_TYPES_T_DML
+
+
+-->>  add  IS_SERVICE_ALLOWED  in GN_GLOBAL_PKG
+
+FUNCTION IS_SERVICE_ALLOWED (P_SERVICE_NAME VARCHAR2)
+   RETURN VARCHAR2
+IS
+   V_SERVICE_ALLOWED   VARCHAR2 (100) :='N';
+
+   CURSOR C_SERVICE_ALLOWED
+   IS
+      SELECT 'Y'
+        FROM GN_FUNCTIONS_T
+       WHERE     ACTION = P_SERVICE_NAME
+             AND (   FUNCTION_ID IN
+                        (SELECT DISTINCT FUNCTION_ID
+                           FROM GN_PRIV_GROUPS_FUNCTIONS_T
+                          WHERE GRP_ID IN
+                                   (SELECT DISTINCT GRP_ID
+                                      FROM GN_USERS_PRIV_T
+                                     WHERE     USER_ID =
+                                                  GN_GLOBAL_PKG.USER_ID
+                                           AND TRUNC (SYSDATE) BETWEEN EFFECTIVE_START_DATE
+                                                                   AND EFFECTIVE_END_DATE))
+                  OR PARENT_FUNCTION_ID IN
+                        (SELECT DISTINCT FUNCTION_ID
+                           FROM GN_PRIV_GROUPS_FUNCTIONS_T
+                          WHERE GRP_ID IN
+                                   (SELECT DISTINCT GRP_ID
+                                      FROM GN_USERS_PRIV_T
+                                     WHERE     USER_ID =
+                                                  GN_GLOBAL_PKG.USER_ID
+                                           AND TRUNC (SYSDATE) BETWEEN EFFECTIVE_START_DATE
+                                                                   AND EFFECTIVE_END_DATE)));
+BEGIN
+   OPEN C_SERVICE_ALLOWED;
+
+   FETCH C_SERVICE_ALLOWED INTO V_SERVICE_ALLOWED;
+
+   CLOSE C_SERVICE_ALLOWED;
+
+   RETURN V_SERVICE_ALLOWED;
+END;
+
+->>>>>>>>>>>>>>>
